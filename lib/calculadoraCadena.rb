75bc37507873fb37580875b56b4c4b89cada2d85
karen <karen.aguayo@ucb.edu.bo>
2021-05-04 04:36:46 -0400

    tercera prueba

--- lib/calculadoraCadena.rb
+++ lib/calculadoraCadena.rb
@@ -1,3 +1,11 @@
 def calcularCadena(numCa)
-    numCa=3,5
+    if(numCa.split(/\n/)[0].include?"//")
+        delimitadores =((numCa.split(/\n/)[0].gsub"/","").gsub"[","").gsub"]",""
+        arraynums=numCa.split(/\n/)[1].split(Regexp.union(delimitadores.split("")))
+    else
+        arraynums=numCa.split(/[,\s]/)
+    end
+    arraynumsInt=arraynums.map{|i| i.to_i if i.to_i<1001}.compact
+    suma =arraynumsInt.sum()
+    return suma
 end
