a72a1004c3cc00c1a957fbd05fd11ceb9d16ab41
karen <karen.aguayo@ucb.edu.bo>
2021-05-04 04:28:50 -0400

    primera prueba

---
+++ spec/CalculadoraCadena_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe "Calculadora De Cadenas" do
+    it "Deberia devolver el numero 3 para la cadena 3" do
+        expect(calcularCadena("3")).to eq(3)
+    end
+end  
+
+def calcularCadena(numCa)
+    numCa=3
+end
